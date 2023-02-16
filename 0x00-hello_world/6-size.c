a980d1fb325eb6050daa72b2fa6b26c91ce1eb43
Qarlifa <udezuaemmanuel22@gmail.com>
2023-02-16 14:36:42 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * main - A program that print the size of various computer types
- * Return 0 (Successful)
+ * main - Entry point
+ * 
+ * Return: 0 (Successful)
  */
 int main(void)
 {
