243c97045e7c179f1360359f666e86f117434711
Qarlifa <udezuaemmanuel22@gmail.com>
2023-02-21 14:20:11 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "putchar.c"
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+	_putchar('Q');
+	_putchar('A');
+	_putchar('R');
+	_putchar('L');
+	_putchar('I');
+	_putchar('F');
+	_putchar('A');
+
+	return (0);
+}
