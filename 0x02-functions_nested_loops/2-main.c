64d92452665f7d6ba32ed9125b4f1f1a60188c42
Qarlifa <udezuaemmanuel22@gmail.com>
2023-02-21 17:11:40 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code for function that makes 10 times the alphabet.
  *
  * Return: Always 0.
  */
