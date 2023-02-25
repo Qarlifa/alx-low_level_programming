cef6b786c206712d5cd62d6fab36d4f2c0bcff8b
Qarlifa <udezuaemmanuel22@gmail.com>
2023-02-24 16:48:36 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File - main.h
+ * Author - Qarlifa
+ * Description: This header file contains prototypes for all functions used in 0x04-more_functions_nested_loops project.
+ */
+
+int _putchar(char c);
+
+
+
+
+#endif
